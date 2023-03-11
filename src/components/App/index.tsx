3af0576e99cca3fa697ca98d70b1644671744f65
Ashley Holbrook <ashholbrook@gmail.com>
2023-03-10 18:04:44 -0600

    change to private false (#9)

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,12 @@
-import "./styles.css"
-import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom"
-import Routes from '../Routes'
+import "./styles.css";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "../Routes";
 
 export function App() {
-
   return (
     <Router>
       <Routes />
     </Router>
-
-  )
+  );
 }
