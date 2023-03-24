84177fc44a7a07981e5929492b63fd4096488a3d
Kenneth Swoger <kenneth_swoger@external.mckinsey.com>
2023-03-24 15:07:44 -0500

    Switch (#13)
    * switch comp
    
    * fixing switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,15 +1,17 @@
-import * as React from "react";
 import Switch from "@mui/material/Switch";
+import CustomContainer from "../CustomContainer";
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
 export default function BasicSwitches() {
   return (
-    <div>
-      <Switch {...label} defaultChecked />
-      <Switch {...label} />
-      <Switch {...label} disabled defaultChecked />
-      <Switch {...label} disabled />
-    </div>
+    <CustomContainer>
+      <div>
+        <Switch {...label} defaultChecked />
+        <Switch {...label} />
+        <Switch {...label} disabled defaultChecked />
+        <Switch {...label} disabled />
+      </div>
+    </CustomContainer>
   );
 }
