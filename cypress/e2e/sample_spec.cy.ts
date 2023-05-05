311917f1dd221d3382d35e3343bfac45d1f3e087
Kenneth Swoger <kenneth_swoger@external.mckinsey.com>
2023-05-05 11:09:08 -0500

    update cypress.config.ts localhost (#20)
    * update cypress.config.ts localhost
    
    * update cy.vist

--- cypress/e2e/sample_spec.cy.ts
+++ cypress/e2e/sample_spec.cy.ts
@@ -1,18 +1,12 @@
 describe("App", function () {
   it("Contains The Internet by Crema", function () {
-    cy.visit("/");
+    cy.visit("/#");
     cy.contains("The Internet by Crema");
-
-    cy.get(".MuiButton-label").click;
     cy.contains("Checkboxes ");
-    //cy.get(".id").contains("The Internet by Crema")
-    //cy.get(".App-link").should("have.attr", "href", "https://reactjs.org")
   });
 
   it("Contains Broken images", function () {
-    cy.visit("/brokenimages");
+    cy.visit("/#/brokenimages");
     cy.contains("Broken Images");
-    //cy.get(".id").contains("The Internet by Crema")
-    //cy.get(".App-link").should("have.attr", "href", "https://reactjs.org")
   });
 });
