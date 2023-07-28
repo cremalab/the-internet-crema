bb983957a4ffe788c673fb9352afb49c8a4524df
Kenneth Swoger <kenneth_swoger@external.mckinsey.com>
2023-07-28 14:47:29 -0500

    GitHub actions test (#28)
    * update cypress.config.ts localhost
    
    * test
    
    * tesing
    
    * yml test
    
    * update yml env
    
    * update package
    
    * yml update
    
    * more updates
    
    * update test url
    
    * update url again
    
    * update return to main
    
    * update again
    
    ---------
    
    Co-authored-by: Ashley Holbrook <Ashley_Holbrook@external.mckinsey.com>

--- cypress/e2e/autocomplete.spec.cy.ts
+++ cypress/e2e/autocomplete.spec.cy.ts
@@ -1,37 +1,37 @@
 beforeEach(function () {
   cy.visit("/#");
   cy.contains("Autocomplete").should("exist").click();
-  cy.url().should('eq', 'http://localhost:5173/the-internet-crema/#/autocomplete');
+  cy.url().should("contain", "/the-internet-crema/#/autocomplete");
 });
 
 it("autocomplete dropdown", function () {
   cy.get('[data-testid="ArrowDropDownIcon"]').click();
-  cy.contains('Juno').click();
-  cy.get('[value="Juno (2007)"]').should('exist');
-  cy.contains('Movie: Juno (2007)').should('exist');
+  cy.contains("Juno").click();
+  cy.get('[value="Juno (2007)"]').should("exist");
+  cy.contains("Movie: Juno (2007)").should("exist");
   cy.reload();
-  cy.get('[value="Juno (2007)"]').should('exist');
-  cy.contains('Movie: Juno (2007)').should('exist');
-  cy.get('[value="Juno (2007)"]').click()  ;
+  cy.get('[value="Juno (2007)"]').should("exist");
+  cy.contains("Movie: Juno (2007)").should("exist");
+  cy.get('[value="Juno (2007)"]').click();
   cy.get('[data-testid="CloseIcon"]').click();
-  cy.contains('Autocomplete').click();
-  cy.contains('Movie: Juno (2007)').should('not.exist');
+  cy.contains("Autocomplete").click();
+  cy.contains("Movie: Juno (2007)").should("not.exist");
 });
 
 it("autocomplete type", function () {
-  cy.get('input[type="text"]').type('Juno{downArrow}{enter}');
-  cy.get('[value="Juno (2007)"]').should('exist');
-  cy.contains('Movie: Juno (2007)').should('exist');
+  cy.get('input[type="text"]').type("Juno{downArrow}{enter}");
+  cy.get('[value="Juno (2007)"]').should("exist");
+  cy.contains("Movie: Juno (2007)").should("exist");
   cy.reload();
-  cy.get('[value="Juno (2007)"]').should('exist');
-  cy.contains('Movie: Juno (2007)').should('exist') ;
+  cy.get('[value="Juno (2007)"]').should("exist");
+  cy.contains("Movie: Juno (2007)").should("exist");
   cy.get('[value="Juno (2007)"]').click();
   cy.get('[data-testid="CloseIcon"]').click();
-  cy.contains('Autocomplete').click();
-  cy.contains('Movie: Juno (2007)').should('not.exist');
+  cy.contains("Autocomplete").click();
+  cy.contains("Movie: Juno (2007)").should("not.exist");
 });
 
 it("return to main", function () {
-  cy.contains('The Internet by Crema').click();
-  cy.url().should('eq', 'http://localhost:5173/the-internet-crema/#/');
+  cy.contains("The Internet by Crema").click();
+  cy.url().should("contain", "/the-internet-crema/#/");
 });
